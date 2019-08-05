eacd0bcfac2b9917a6cf4f45bc4c04b16254169a
Marta Sadova <sadovamarta3@ukr.net>
2019-08-05 14:21:26 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    var options = { 
-        year: 'numeric', 
-        month: 'long', 
+    var options = {
+        year: 'numeric',
+        month: 'long',
         day: 'numeric',
         time: 'numeric'
     };
@@ -14,14 +14,18 @@ $(document).ready(function () {
     var dateString = date.toDateString();
 
     $(".date").html(date1);
-    $('.time').html(hour + ':' + minute);
-        $('#dateAndTime').html(dateString + ' ' + hour + ':' + minute);
+    if (minute < 10) {
+        $('.time').html(hour + ':0' + minute);
+    } else {
+        $('.time').html(hour + ':' + minute);
+    }
+    $('#dateAndTime').html(dateString + ' ' + hour + ':' + minute);
 
     $.ajax({
         url: 'http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19',
         type: "GET",
         dataType: "json",
-        success: function(data){
+        success: function(data) {
             var widget = show(data);
             console.log(data);
             $('.parameters').html(widget);
@@ -29,20 +33,19 @@ $(document).ready(function () {
             var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
             $('#icon').attr('src', iconUrl);
             $('#temp').html(data.main.temp + '&deg;C');
-            $('.parameters2').html("<p>Feels like: " +data.main.temp+ '&deg;C' + "</p>" +
-                "<p style='font-size: 20px; font-weight: bold; text-transform: capitalize; '>" +data.weather[0].description+ "</p>")
+            $('.parameters2').html("<p>Feels like: " + data.main.temp + '&deg;C' + "</p>" +
+                "<p style='font-size: 20px; font-weight: bold; text-transform: capitalize; '>" + data.weather[0].description + "</p>")
         }
     });
 
     function show(data) {
-        return  "<br><p>Humidity: " +data.main.humidity+ "%" +"</p>"+
-                "<p>Pressure: " +data.main.pressure+ " hPa" +"</p>" +
-                "<p style='line-height: 50px;'>Wind: " +data.wind.speed+ " km/h SSE" +"</p>"
+        return "<br><p>Humidity: " + data.main.humidity + "%" + "</p>" +
+            "<p>Pressure: " + data.main.pressure + " hPa" + "</p>" +
+            "<p style='line-height: 50px;'>Wind: " + data.wind.speed + " km/h SSE" + "</p>"
     }
-    
-    $(".refresh_link").click(function(){
+
+    $(".refresh_link").click(function() {
         location.reload();
     });
-    
-});
 
+});
