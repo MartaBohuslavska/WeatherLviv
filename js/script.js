67d003a91ff46dd36b27475372bc08d47e8e6903
Marta Sadova <sadovamarta3@ukr.net>
2019-08-05 14:44:21 +0300

    third commit

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
 
     $.ajax({
         url: 'http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19',
-        type: "GET",
+        type: "POST",
         dataType: "json",
         success: function(data) {
             var widget = show(data);
